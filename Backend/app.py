576e78f1201e36ead793dcccc12615134f0dad82
brendanzheng <brendan.zheng02@gmail.com>
2023-11-18 17:12:04 -0500

    basic backend implementation

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,195 @@
 import json
 
-import db
-from flask import Flask
-from flask import request
-
-DB = db.DatabaseDriver()
+from db import db, User, Service
+from flask import Flask, request
 
 app = Flask(__name__)
+db_filename = "quotas.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
+def success_response(body, code=200):
+    """
+    Abstraction for a success response
+    """
+    return json.dumps(body), code
+
+
+def failure_response(message, code=404):
+    """
+    Abstraction for a failure response
+    """
+    return json.dumps({"error": message}), code
+
+
+# Routes
+
+
+# Users
 @app.route("/")
-def hello_world():
-    return "Hello world!"
+@app.route("/api/users/")
+def get_all_users():
+    """
+    Endpoint for getting all users
+    """
+    users = []
+    for user in User.query.all():
+        users.append(user.serialize())
+    return success_response({"users": users})
+
+
+@app.route("/api/users/<int:user_id>/")
+def get_a_user(user_id):
+    """
+    Endpoint for getting a user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    return success_response(user.serialize())
+
+
+@app.route("/api/users/", methods=["POST"])
+def create_a_user():
+    """
+    Endpoint for creating a user
+    """
+    body = json.loads(request.data)
+    user_username = body.get("username")
+    user_quotas_left = body.get("quotas_left")
+    if user_username is None or user_quotas_left is None:
+        return failure_response("Invalid fields provided", 400)
+    new_user = User(username=user_username, quotas_left=user_quotas_left)
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+
+@app.route("/api/users/<int:user_id>/", methods=["POST"])
+def update_a_user(user_id):
+    """
+    Endpoint for updating a user
+    """
+    body = json.loads(request.data)
+    user_username = body.get("username")
+    user_quotas_left = body.get("quotas_left")
+    if user_username is None or user_quotas_left is None:
+        return failure_response("Invalid fields provided", 400)
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    user.username = user_username
+    user.quotas_left = user_quotas_left
+    db.session.commit()
+    return success_response(user.serialize())
+
+
+@app.route("/api/users/<int:user_id>/", methods=["DELETE"])
+def delete_a_user(user_id):
+    """
+    Endpoint for deleting a user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+
+# Services
+
+
+@app.route("/api/services/")
+def get_all_services():
+    """
+    Endpoint for getting all services
+    """
+    services = []
+    for service in Service.query.all():
+        services.append(service.serialize())
+    return success_response({"services": services})
+
+
+@app.route("/api/services/<int:service_id>/")
+def get_a_service(service_id):
+    """
+    Endpoint for getting a service by id
+    """
+    service = Service.query.filter_by(id=service_id).first()
+    if service is None:
+        return failure_response("Service not found")
+    return success_response(service.serialize())
+
+
+@app.route("/api/services/", methods=["POST"])
+def create_a_service():
+    """
+    Endpoint for creating a service
+    """
+    body = json.loads(request.data)
+    service_name = body.get("name")
+    service_description = body.get("description")
+    service_quotas_required = body.get("quotas_required")
+    if (
+        service_name is None
+        or service_description is None
+        or service_quotas_required is None
+    ):
+        return failure_response("Invalid fields provided", 400)
+    new_service = Service(
+        name=service_name,
+        description=service_description,
+        quotas_required=service_quotas_required,
+    )
+    db.session.add(new_service)
+    db.session.commit()
+    return success_response(new_service.serialize(), 201)
+
+
+@app.route("/api/services/<int:service_id>/", methods=["POST"])
+def update_a_service(service_id):
+    """
+    Endpoint for updating a service
+    """
+    body = json.loads(request.data)
+    service_name = body.get("name")
+    service_description = body.get("description")
+    service_quotas_required = body.get("quotas_required")
+    if (
+        service_name is None
+        or service_description is None
+        or service_quotas_required is None
+    ):
+        return failure_response("Invalid fields provided", 400)
+    service = Service.query.filter_by(id=service_id).first()
+    if service is None:
+        return failure_response("Service not found")
+    service.name = service_name
+    service.description = service_description
+    service.quotas_required = service_quotas_required
+    db.session.commit()
+    return success_response(service.serialize())
+
+
+@app.route("/api/services/<int:service_id>/", methods=["DELETE"])
+def delete_a_service(service_id):
+    """
+    Endpoint for deleting a service by id
+    """
+    service = Service.query.filter_by(id=service_id).first()
+    if service is None:
+        return failure_response("Service not found")
+    db.session.delete(service)
+    db.session.commit()
+    return success_response(service.serialize())
 
 
 if __name__ == "__main__":
--- Backend/db.py
+++ Backend/db.py
@@ -1,23 +1,101 @@
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+user_service_assoc_table = db.Table(
+    "user_service_assoc_table",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("service_id", db.Integer, db.ForeignKey("service.id")),
+)
 
 
-# From: https://goo.gl/YzypOI
-def singleton(cls):
-    instances = {}
+class User(db.Model):
+    """
+    Users model
+    """
 
-    def getinstance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, nullable=False)
+    quotas_left = db.Column(db.Integer, nullable=False)
+    services = db.relationship(
+        "Service",
+        secondary=user_service_assoc_table,
+        back_populates="users",
+    )
 
-    return getinstance
+    def __init__(self, **kwargs):
+        """
+        Initialize a user object
+        """
+        self.username = kwargs.get("username")
+        self.quotas_left = kwargs.get("quotas_left")
+
+    def serialize(self):
+        """
+        Serializes a user object
+        """
+        return {
+            "id": self.id,
+            "username": self.username,
+            "quotas_left": self.quotas_left,
+            "services": [s.serialize_basic_info() for s in self.services],
+        }
+
+    def serialize_basic_info(self):
+        """
+        Serializes a user object without information about their number of
+        remaining quotas or services
+        """
+        return {
+            "id": self.id,
+            "username": self.username,
+        }
 
 
-class DatabaseDriver(object):
-    def __init__(self):
-        pass
+class Service(db.Model):
+    """
+    Service model
+    """
 
+    __tablename__ = "service"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    quotas_required = db.Column(db.Integer, nullable=False)
+    users = db.relationship(
+        "User",
+        secondary=user_service_assoc_table,
+        back_populates="services",
+    )
 
-# Only <=1 instance of the database driver
-# exists within the app at all times
-DatabaseDriver = singleton(DatabaseDriver)
+    def __init__(self, **kwargs):
+        """
+        Initialize a service object
+        """
+        self.name = kwargs.get("name")
+        self.description = kwargs.get("description")
+        self.quotas_required = kwargs.get("quotas_required")
+
+    def serialize(self):
+        """
+        Serializes a service object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "quotas_required": self.quotas_required,
+            "users": [u.serialize_basic_info() for u in self.users],
+        }
+
+    def serialize_basic_info(self):
+        """
+        Serializes a service object without information about users
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "quotas_required": self.quotas_required,
+        }
