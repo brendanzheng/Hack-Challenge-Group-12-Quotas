ff2548e69c998bdda49b0c61631e2efea0c64575
willyliu1705 <will.liu1705@gmail.com>
2023-11-18 23:35:13 -0500

    starter code

---
+++ FrontEnd/iOS Project/Extensions/UIColor+.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+.swift
+//  iOS Project
+//
+//  Created by William Liu on 11/18/23.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let aFinal = AFinal()
+
+    struct AFinal {
+
+        let offWhite = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        let silver = UIColor(red: 191/255, green: 191/255, blue: 191/255, alpha: 1)
+        let yellowOrange = UIColor(red: 255/255, green: 170/255, blue: 51/255, alpha: 1)
+        let ruby = UIColor(red: 202/255, green: 66/255, blue: 56/255, alpha: 1)
+        
+    }
+
+}
