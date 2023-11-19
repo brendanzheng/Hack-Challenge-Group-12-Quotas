f96e648bbba14270c13e41361ea5689df8ec9c1e
willyliu1705 <127636492+willyliu1705@users.noreply.github.com>
2023-11-18 21:51:01 -0500

    Add files via upload

---
+++ FrontEnd/iOS Project/iOS Project/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  iOS Project
+//
+//  Created by William Liu on 11/18/23.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    // MARK: - Properties (data)
+    
+    // MARK: - init
+    
+}
