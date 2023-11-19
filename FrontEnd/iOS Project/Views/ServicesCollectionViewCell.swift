19954cc6587f1ec54303eeaee48367638fb08172
willyliu1705 <127636492+willyliu1705@users.noreply.github.com>
2023-11-18 21:54:24 -0500

    Add files via upload

---
+++ FrontEnd/iOS Project/Views/ServicesCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServicesCollectionViewCell.swift
+//  iOS Project
+//
+//  Created by William Liu on 11/18/23.
+//
+
+import UIKit
+import SDWebImage
+import SnapKit
+
+class ServicesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties (view)
+    
+    // MARK: - Properties (data)
+    
+    // MARK: - init
+    
+    // MARK: - configure
+    
+    // MARK: - Set Up Views
+    
+}
